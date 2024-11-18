6f02bfd61c2ce7ab7f80921d21bc6a1986a94116
Ahmed Kandeel <1kandeel1@gmail.com>
2024-11-18 13:14:55 +0200

    Make Migrations run with startup

--- API/Program.cs
+++ API/Program.cs
@@ -27,4 +27,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.MigrateDatabase();
+
 app.Run();
---
+++ Persistence/MigrationManager.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RegistrationFormApi.Persistence
+{
+    public static class MigrationManager
+    {
+        public static WebApplication MigrateDatabase(this WebApplication webApp)
+        {
+            using (var scope = webApp.Services.CreateScope())
+            {
+                using (var appContext = scope.ServiceProvider.GetRequiredService<AppDbContext>())
+                {
+                    try
+                    {
+                        appContext.Database.Migrate();
+                        Console.WriteLine("Migration Done");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Migration Failed");
+                        Console.WriteLine(ex);
+                        throw;
+                    }
+                }
+            }
+            return webApp;
+        }
+    }
+}
